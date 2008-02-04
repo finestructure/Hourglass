1f1dba5982d842909e8d632ada509fb5631eb2de
sas <sas@bfea69f6-0cce-0310-9b16-88a7a1f2502c>
2008-02-04 14:20:50 +0000

    Added AppController to handle Untitled document popup.
    git-svn-id: https://dev.abstracture.de/repos/Import/sas/Projects/Private/PdbX@2953 bfea69f6-0cce-0310-9b16-88a7a1f2502c

---
+++ AppController.h
@@ -0,0 +1,16 @@
+//
+//  AppController.h
+//  PdbX
+//
+//  Created by Sven A. Schmidt on 31.01.08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface AppController : NSObject {
+
+}
+
+@end
---
+++ AppController.m
@@ -0,0 +1,22 @@
+//
+//  AppController.m
+//  PdbX
+//
+//  Created by Sven A. Schmidt on 31.01.08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppController.h"
+
+
+@implementation AppController
+
+// ----------------------------------------------------------------------
+
+- (BOOL) applicationShouldOpenUntitledFile:(NSApplication*)app {
+  return YES;
+}
+
+// ----------------------------------------------------------------------
+
+@end
