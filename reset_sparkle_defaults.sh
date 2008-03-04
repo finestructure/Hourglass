736c9a5ea21576200bbc4888b1df9791b8b8309b
sas <sas@bfea69f6-0cce-0310-9b16-88a7a1f2502c>
2008-03-04 12:00:08 +0000

    Merged sparkle branch.
    git-svn-id: https://dev.abstracture.de/repos/Projects/sas/Hourglass@3029 bfea69f6-0cce-0310-9b16-88a7a1f2502c

---
+++ reset_sparkle_defaults.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+defaults delete de.abstracture.Hourglass SUCheckAtStartup
+defaults delete de.abstracture.Hourglass SULastCheckTime
+defaults delete de.abstracture.Hourglass "NSWindow Frame SUUpdateAlertFrame"
